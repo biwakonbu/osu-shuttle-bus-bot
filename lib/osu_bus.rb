f2bb104b65bcea31294b9d998a8ede38503ccf44
Ryo HIGASIGAWA <ryo.higasigawa@gmail.com>
2013-07-31 18:00:26 +0900

    test/osu_bus
    test code get() method.
    check respons status code '200'.

--- lib/osu_bus.rb
+++ lib/osu_bus.rb
@@ -1,10 +1,22 @@
-class OSUBus
-  attr_reader :url
+require 'open-uri'
+require 'nokogiri'
+require 'twitter'
+require 'yaml'
+require 'time'
 
-  def initialize()
+class OSUBus
+  attr_reader :url, :doc
+
+  def initialize
     @url = 'http://www.osaka-sandai.ac.jp/cgi-bin/cms/campus_life.cgi?studentlife_cd=5JzeUsFN0i'
   end
-  
+
+  def get
+    respons = open(@url)
+    @doc = Nokogiri::HTML(respons)
+    respons.status[0]
+  end
+
   def tweet
     "Hello RSpec"
   end
--- test/osu_spec.rb
+++ test/osu_spec.rb
@@ -4,13 +4,18 @@ require 'osu_bus'
 
 describe "OSU Shuttle Bus" do
   it "should say 'Hello RSpec' When it receives the osu() message" do
-    osu_bus = OSUBus.new
-    tweeting = osu_bus.tweet
+    bus = OSUBus.new
+    tweeting = bus.tweet
     tweeting.should == "Hello RSpec"
   end
 
   it "should set URL when it create OSUBus object" do
     bus = OSUBus.new
     bus.url == 'http://www.osaka-sandai.ac.jp/cgi-bin/cms/campus_life.cgi?studentlife_cd=5JzeUsFN0i'
+  end
+
+  it "should get HTTP Respons '200' when GET URI Page get() respons code" do
+    bus = OSUBus.new
+    bus.get == '200'
   end
 end
